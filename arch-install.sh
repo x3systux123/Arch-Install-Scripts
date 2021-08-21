1a0b505c56bb7832d0f038da0fcf85ec2034c2f7
x3systux123 <89300343+x3systux123@users.noreply.github.com>
2021-08-21 13:05:58 +0200

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -6,15 +6,18 @@ echo;
 echo;
 #list all hard drives in your system
 lsblk;
+echo;
 echo select the drive for the system: "/dev/sd";
 read hardrive;
 cfdisk $hardrive;
 brake;
 clear;
 lsblk;
+echo;
 echo please select your swap partition;
 read swap;
 mkswap -L swap $swap;
+echo;
 echo please select your root partition;
 read root;
 mkfs.ext4 -L root $root;
@@ -23,6 +26,7 @@ echo now we can install the base system;
 mount -L root /mnt;
 swapon -L swap;
 pacstrap /mnt base base-devel linux linux-firmware dhcpcd nano;
+echo;
 echo -n "Enter your ucode: "
 echo "1) intel-ucode"
 echo "2) amd-ucode"
@@ -38,5 +42,6 @@ genfstab -U /mnt > /mnt/etc/fstab;
 pacman -Sy git;
 git clone https://github.com/x3systux123/Arch-Install-Scripts.git;
 cd Arch-Install-Scripts;
-mv arch-chroot-install.sh /mnt/root/;
+mv arch-chroot-install.sh /mnt/;
+echo export PS1="[chroot] ${PS1}" > /mnt/root/.bashrc
 arch-chroot /mnt;
