5a99d382cfd372361ec8629d11bcca0b6d5812bd
x3systux123 <89300343+x3systux123@users.noreply.github.com>
2021-08-21 12:54:46 +0200

    Add files via upload

---
+++ arch-chroot-install.sh
@@ -0,0 +1,54 @@
+echo "please select your hostname: ";
+read hostname;
+echo $hostname > /etc/hostname;
+clear;
+echo LANG=de_DE.UTF-8 > /etc/locale.conf;
+echo en_US.UTF-8 UTF-8 > /etc/locale.gen;
+locale-gen;
+echo "please tipe your Timezone z.B Berlin,Amsterdam etc. : "
+read timezone;
+ln -sf /usr/share/zoneinfo/Europe/$timezone /etc/localtime;
+clear;
+echo [multilib] > /etc/pacman.conf;
+echo SigLevel = PackageRequired TrustedOnly > /etc/pacman.conf;
+echo Include = /etc/pacman.d/mirrorlist > /etc/pacman.conf;
+clear;
+mkinitcpio -p linux;
+passwd;
+clear;
+pacman -S grub;
+grub-install $hardrive;
+gtub-mkconfig -o /boot/grub/grub.cfg;
+clear;
+echo "now we create your user: ";
+read $user;
+useradd -m -g users -s /bin/bash $user;
+passwd $user;
+gpasswd -a $user video;
+gpasswd -a $user audio;
+gpasswd -a $user wheel;
+echo "%wheel ALL=(ALL) ALL" > /etc/sudoers;
+systemctl enable --now fstrim.timer;
+pacman -S acpid dbus avahi cups;
+systemctl enable acpid;
+systemctl enable avahi-daemon;
+systemctl enable cups.service;
+systemctl enable --now systemd-timesyncd.service;
+echo -n "Enter your Videodriver: "
+echo "1) nvidia"
+echo "2) amd"
+read videodriver
+if [ "$videodriver" = "nvidia" ] than
+    pacman -S nvidia nvidia-settings
+else
+    pacman -S xf86-video-amdgpu
+fi;
+pacman -S plasma plasma-desktop plasma-meta dolphin konsole;
+systemctl enable sddm;
+exit;
+umount -R /mnt;
+swapoff -L swap;
+clear;
+reboot;
+
+
---
+++ arch-install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo
+echo This is are Arch Linux install script made by x3!;
+echo;
+echo;
+echo;
+#list all hard drives in your system
+lsblk;
+echo select the drive for the system: "/dev/sd";
+read hardrive;
+cfdisk $hardrive;
+brake;
+clear;
+lsblk;
+echo please select your swap partition;
+read swap;
+mkswap -L swap $swap;
+echo please select your root partition;
+read root;
+mkfs.ext4 -L root $root;
+clear;
+echo now we can install the base system;
+mount -L root /mnt;
+swapon -L swap;
+pacstrap /mnt base base-devel linux linux-firmware dhcpcd nano;
+echo -n "Enter your ucode: "
+echo "1) intel-ucode"
+echo "2) amd-ucode"
+read ucode
+if [[ $ucode -gt 1 ]]
+then
+    pacman --root /mnt -S amd-ucode
+else
+    pacman --root /mnt -S intel-ucode
+fi;
+clear;
+genfstab -U /mnt > /mnt/etc/fstab;
+pacman -Sy git;
+git clone https://github.com/x3systux123/Arch-Install-Scripts.git;
+cd Arch-Install-Scripts;
+mv arch-chroot-install.sh /mnt/root/;
+arch-chroot /mnt;
